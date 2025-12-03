5f21a758e2573e045e319a343c3959a99644e4f9
dkphhh <dkphhh@foxmail.com>
2025-12-03 20:51:37 +0800

    update

--- src/pages/formCreate/App.svelte
+++ src/pages/formCreate/App.svelte
@@ -1,69 +1,18 @@
 <script lang="ts">
 	import Layout from '@/components/layout/Layout.svelte';
 	import SheetFormCreate from '@/components/forms/sheet/SheetFormCreate.svelte';
-	import BitableFormSetting from '@/components/forms/bitable/BitableFormSetting.svelte';
-	import DocFormSetting from '@/components/forms/feishuDoc/DocFormSetting.svelte';
-
+	import BitableFormCreate from '@/components/forms/bitable/BitableFormCreate.svelte';
+	import DocFormCreate from '@/components/forms/feishuDoc/DocFormCreate.svelte';
 	const searchParams = new URL(window.location.toString()).searchParams;
 	const formTypeName = searchParams.get('type') as FormTypeName;
-
-	let createForm: FormType = $state(
-		(() => {
-			if (formTypeName === '电子表格') {
-				return {
-					formType: '电子表格',
-					fields: ['title', 'url'],
-					id: crypto.randomUUID(),
-					name: '',
-					rangeIndex: {
-						endIndex: '',
-						startIndex: ''
-					},
-					sheetId: '',
-					sheetToken: ''
-				};
-			}
-
-			if (formTypeName === '多维表格') {
-				return {
-					formType: '多维表格',
-					id: crypto.randomUUID(),
-					name: '',
-					appToken: '',
-					tableId: '',
-					fieldsMap: {
-						title: undefined,
-						author: undefined,
-						description: undefined,
-						published: undefined,
-						source: undefined,
-						url: undefined,
-						feishuDocUrl: undefined
-					}
-				};
-			}
-
-			if (formTypeName === '飞书文档') {
-				return {
-					formType: '飞书文档',
-					id: crypto.randomUUID(),
-					name: '',
-					folderToken: ''
-				};
-			}
-
-			alert('未知的表单类型');
-			throw new Error('未知的表单类型');
-		})()
-	);
 </script>
 
 <Layout>
 	{#if formTypeName == '电子表格'}
 		<SheetFormCreate />
 	{:else if formTypeName == '多维表格'}
-		<BitableFormSetting form={createForm as BitableFormType} />
+		<BitableFormCreate />
 	{:else if formTypeName == '飞书文档'}
-		<DocFormSetting form={createForm as DocFromType} />
+		<DocFormCreate />
 	{/if}
 </Layout>
